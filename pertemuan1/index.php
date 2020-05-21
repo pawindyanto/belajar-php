add6aaaa41954d74c212ae790bde4926f020fb23
pawindyanto <pawindyanto@gmail.com>
2020-05-21 22:42:15 +0700

    tambah data mahasiswa

--- pertemuan1/index.php
+++ pertemuan1/index.php
@@ -4,12 +4,32 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Daftar Mahasiswa</title>
 </head>
 
 <body>
-  <h1>UJI COBA SAJA 1</h1>
-  <h2>Saya danang</h2>
+  <h3>Dafftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="./img/sandhika.jpeg"></td>
+      <td>12345</td>
+      <td>Danang</td>
+      <td>Pawindyanto@gmail.com</td>
+      <td>Teknik Komputer</td>
+      <td><a href="">Ubah</a> / <a href="">Hapus</a></td>
+    </tr>
+  </table>
 </body>
 
 </html>
